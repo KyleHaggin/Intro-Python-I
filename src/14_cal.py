3d719ae0c727bdf8d0aec2f75c1fd9eaf3c0ed37
KyleHaggin <KyleHaggin@users.noreply.github.com>
2020-02-10 13:50:03 -0800

    finished 14

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,36 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
-prompted input. Also, the brackets around year are to denote that the argument is
+Note: the user should provide argument input
+(in the initial call to run the file) and not
+prompted input. Also, the brackets around year are to
+denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you
+would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if
+you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+if len(sys.argv) == 1:
+    year = datetime.now().year
+    month = datetime.now().month
+    print(month)
+elif len(sys.argv) == 2:
+    year = datetime.now().year
+    month = int(sys.argv[1])
+elif len(sys.argv) == 3:
+    year = int(sys.argv[2])
+    month = int(sys.argv[1])
+else:
+    print('Invald command line input detected.\n'
+          'Please enter using format:\n14_cal.py [month] [year]')
+    sys.exit()
+
+calendar.TextCalendar().prmonth(year, month)
